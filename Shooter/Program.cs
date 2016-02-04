d192690d1d750bb2f331612edc33b93c263051eb
Robert Woods <robertjawoods@gmail.com>
2016-02-04 17:26:23 +0000

    Renamed Game1 to Shooter

--- Shooter/Program.cs
+++ Shooter/Program.cs
@@ -14,7 +14,7 @@ namespace Shooter
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Shooter())
                 game.Run();
         }
     }
